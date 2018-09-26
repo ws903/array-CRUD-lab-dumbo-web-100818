fe587d8e177dab0ad0914f66f96c900b305353ec
David Sim <wsim903@gmail.com>
2018-09-26 18:41:27 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,11 +11,12 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array.push(element)
+  array
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
